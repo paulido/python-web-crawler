34869017f7d07ee0f24429fa3a3d9e8765113e7a
Paul IDO <paulido92@gmail.com>
2022-08-13 02:16:47 +0000

    add settings

--- interface/interface/settings.py
+++ interface/interface/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-*64-2f)p#w(0vb)1(3v)bk3+2rut5e@ncox=-spijx^zcs8ffl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.jon(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -128,4 +133,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-DISABLE_COLLECTSTATIC=1
+
